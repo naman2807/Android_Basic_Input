7eadc467b06cc9c231d38535edf665efbfd9ef9e
naman2807 <namanagarwaldbs438@gmail.com>
2021-07-03 12:19:11 +0530

    Declared kmText EditText

--- app/src/main/java/com/example/basicinput/MainActivity.java
+++ app/src/main/java/com/example/basicinput/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.basicinput;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button kmToMilesButton = findViewById(R.id.buttonConvKmToMiles);
         Button milesToKmButton = findViewById(R.id.buttonConvMilesToKm);
+        EditText kmText = findViewById(R.id.kiloInput);
 
     }
 }
